f614d9bc1e83af0433d06ac11355cef2fc3801af
Cole <cole.samuelsen@kehe.com>
2023-02-06 10:52:26 -0600

    ability to switch color

--- Javascript/sprites.js
+++ Javascript/sprites.js
@@ -6,3 +6,7 @@ let characterIdleHeight = 40;
 
 let characterIdlePNG = new Image();
 characterIdlePNG.src = "Images/Sprites/CHARACTER_SPRITES/Blue/Gunner_Blue_Idle.png";
+
+function changeCharacterColor(color) {
+    characterIdlePNG.src = `Images/Sprites/CHARACTER_SPRITES/${color}/Gunner_${color}_Idle.png`;
+}
