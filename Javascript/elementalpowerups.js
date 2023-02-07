85450629bf0833f4712ad58f004cd8adece4c575
Cole <cole.samuelsen@kehe.com>
2023-02-07 09:52:51 -0600

    fix

--- Javascript/elementalpowerups.js
+++ Javascript/elementalpowerups.js
@@ -5,23 +5,17 @@ const ElementalPowerUps = {
 }
 
 function ApplyDamage(victim, damage) {
-    console.log("Trying to apply burn");
-    if (victim.burnsLeft <= 0) { return; }
     victim.health -= damage;
-    victim.burnsLeft -= 1;
-    
 }
 
 function Burn(victim, damage, timeInSeconds) {
     let timeInMs = timeInSeconds * 1000;
     let timePerTick = timeInMs / burnTicks;
-    victim.burnsLeft = burnTicks;
-    let intervalId = setInterval(function () {
-        ApplyDamage(victim, damage / burnTicks);
-        if (victim.burnsLeft <= 0 || victim.health <= 0) {
-            clearInterval(intervalId);
-        }
-    }, timePerTick);
+    for (var i = 0; i < burnTicks; i++) {
+        setTimeout(function () {
+            ApplyDamage(victim, damage / burnTicks);
+        }, i * timePerTick);
+    }
 }
 
 function ApplyPowerUp(victim, powerUpType, damage) {
@@ -29,7 +23,6 @@ function ApplyPowerUp(victim, powerUpType, damage) {
         case ElementalPowerUps.Fire: {
             Burn(victim, damage, 5);
         }
-
         default:
     }
 }
