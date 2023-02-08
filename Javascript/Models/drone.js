89f06dac7db86453929f4b7175f8899d802c227a
Cole <cole.samuelsen@kehe.com>
2023-02-08 14:37:00 -0600

    improved drone smoothness

--- Javascript/Models/drone.js
+++ Javascript/Models/drone.js
@@ -10,6 +10,7 @@ class Drone extends BaseObject {
         this.hoverDistance = 100;
         this.ally = null;
         this.speed = 3;
+        this.distanceToAlly = 0;
     }
     update(ally) {
         this.ally = ally;
@@ -23,16 +24,18 @@ class Drone extends BaseObject {
 
         this.angle = Math.atan2(character.y + character.height / 2 - (this.y + this.height / 2), character.x + character.width / 2 - (this.x + this.width / 2));
 
-        this.x = this.x + this.width / 2 + this.speed / 1.5 * Math.cos(this.angle) - this.width / 2;
+        let speed = this.speed * (this.distanceToAlly / 100) - 2;
 
-        this.y = this.y + this.height / 2 + this.speed / 1.5 * Math.sin(this.angle) - this.height / 2;
+        this.x = this.x + this.width / 2 + speed * Math.cos(this.angle) - this.width / 2;
+
+        this.y = this.y + this.height / 2 + speed / 1.5 * Math.sin(this.angle) - this.height / 2;
     }
 
     handleMovement() {
 
-        let distanceToCharacter = calculateDistance(this.x, this.y, this.ally.x, this.ally.y);
+        this.distanceToAlly = calculateDistance(this.x, this.y, this.ally.x, this.ally.y);
 
-        if (distanceToCharacter > this.hoverDistance) {
+        if (this.distanceToAlly > this.hoverDistance) {
             this.moveTowardsAlly();
         }
 
